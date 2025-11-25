4c3fa1a91bfbd9d0378c63095f3f1d784e0cbcd8
Eusidcarrillo <161573528+Eusidcarrillo@users.noreply.github.com>
2025-11-25 02:14:58 -0500

    Agregar archivos de proyecto.

---
+++ formSub1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinanzIA
+{
+    public partial class formSub1 : Form
+    {
+        public formSub1()
+        {
+            InitializeComponent();
+        }
+
+        private void formSub1_Load(object sender, EventArgs e)
+        {
+            this.ControlBox = false;
+        }
+    }
+}
